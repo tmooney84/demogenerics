6710e3f85a34fd9a1ff7cb677d6fbf558a0f16b2
Trevor <trevordmooney@gmail.com>
2024-06-03 13:01:00 -0700

    Drafted Exercise

---
+++ myRunner.java
@@ -0,0 +1,17 @@
+public class myRunner {
+    public static void main(String[] args) {
+        // initialize generic class
+        // with String and Integer data
+
+        GMultipleDatatype<String, Integer> mobj = new GMultipleDatatype("Per Scholas", 11025);
+
+        System.out.println(mobj.getValueOne());
+        System.out.println(mobj.getValueTwo());
+
+        // initialize generic class
+        // with String and String data
+        GMultipleDatatype<String, String> mobj2 = new GMultipleDatatype("Per Scholas", "Non profit");
+        System.out.println(mobj2.getValueOne());
+        System.out.println(mobj2.getValueTwo());
+    }
+}
